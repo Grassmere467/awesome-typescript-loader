4bd05b203d8e569af8630307f911814e08cb1472
spyro467 <backup@grassmere-it.com>
2017-04-21 09:08:06 +0100

    Force override of experimentalDecorators

--- src/instance.ts
+++ src/instance.ts
@@ -249,6 +249,9 @@ function applyDefaults(
         compilerConfig.options.isolatedModules = true;
     }
 
+    console.log('Adding experimentalDecorators')
+    compilerConfig.options.experimentalDecorators = true
+
     _.defaults(compilerConfig.options, {
         sourceRoot: compilerConfig.options.sourceMap ? context : undefined
     });
